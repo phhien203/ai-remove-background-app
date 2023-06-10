c72601df81d4523233ee56851c70347855498b78
Pham Huu Hien <phhien203@gmail.com>
2023-06-10 11:18:40 +0700

    feat: display uploaded image

--- app/page.tsx
+++ app/page.tsx
@@ -25,14 +25,27 @@ export default function Home() {
     setFile(acceptedFiles[0]);
   };
 
+  const fileSize = (size: number) => {
+    if (size === 0) {
+      return "0 Bytes";
+    }
+
+    const k = 1024;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(size) / Math.log(k));
+    return parseFloat((size / Math.pow(k, i)).toFixed(2)) + "" + sizes[i];
+  };
+
   return (
     <div className="max-w-3xl mx-auto my-10 px-4">
+      {/* Header Section */}
       <section className="text-center mb-10">
         <h1 className="font-semibold text-transparent text-5xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block bg-clip-text">
           Remove background
         </h1>
       </section>
 
+      {/* Dropzone Section */}
       <section className="w-full max-w-lg mx-auto mb-12">
         <div className="w-full text-center border-4 border-gray-500 border-dashed rounded-md cursor-pointer mb-2 text-gray-500">
           <Dropzone
@@ -64,6 +77,28 @@ export default function Home() {
               Remove background
             </button>
           </div>
+        )}
+      </section>
+
+      <section className="grid grid-cols-2 gap-4 mt-4">
+        {file && (
+          <>
+            <div className="relative">
+              <img
+                src={URL.createObjectURL(file)}
+                alt={file.name}
+                className="w-full h-full object-cover"
+              />
+
+              <div className="absolute left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 text-white text-md p-2">
+                {file.name} ({fileSize(file.size)})
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center">
+              Output image here
+            </div>
+          </>
         )}
       </section>
     </div>
